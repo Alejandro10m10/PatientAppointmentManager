f407bc1ff48542c4d7fb27a309ef59b9e3218c00
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-05-08 22:46:27 -0500

    Add appointment data to list object

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+// Campos del formulario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+// UI
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+// Objeto con la información de la cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: '',
+}
+
+// Registrar eventos
+eventListeners();
+function eventListeners(){
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+}
+
+// Agrega datos al objeto de cita
+function datosCita(e){
+    citaObj[e.target.name] = e.target.value; // El objeto tiene el mismo nombre en sus propiedades que los campos input con el atributo 'name'
+    console.log(citaObj);
+}
